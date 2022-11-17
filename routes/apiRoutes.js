e66a40c6b2db20c980d9d7c3632ca161811158d8
Maryam Muska <maryammuska@ucla.edu>
2022-11-16 21:42:39 -0800

    changing routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,31 @@
 const router = require("express").Router();
-const uuid = require("uuid");
+const uniqid = require("uniqid");
 const fs = require("fs");
+const path = require("path");
 
 // GET Route
 
-router.get("/notes", (req, res) => {
-    let data = fs.readFileSync("./db/db.json");
-    res.json(JSON.parse(data));
-})
-
-router.post("/notes", (req, res) => {
-    let db = JSON.parse(fs.readFileSync("./db/db.json"));
-    let addDb = req.body;
-    addDb.id =uuid.v4();
-    db.push(addDb);
-    fs.writeFileSync("./db/db.json", JSON.stringify(db));
-    res.json(notes);
+router.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../db/db.json"))
 });
 
-router.delete("/notes/:id", (req, res) => {
-    let db = JSON.parse(fs.readFileSync("db/db.json"));
+router.post("/api/notes", (req, res) => {
+    let db = fs.readFileSync("db/db.json");
+    db = JSON.parse(db);
+    res.json(db);
+
+    let dbNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uniqid(),
+    };
+
+    db.push(dbNote);
+    fs.writeFileSync("db/db.json", JSON.stringify(db));
+});
+
+router.delete("/api/notes/:id", (req, res) => {
+    let db = JSON.parse(fs.readFileSync("./db/db.json"));
     let deleteDb = db.filter((item) => item.id !== req.params.id);
     fs.writeFileSync("./db/db.json", JSON.stringify(deleteDb));
 
